5d02ec7b006fdbabebba7ceefa9147bd097981a4
tanzhengtang <tanzhengtang@163.com>
2023-10-26 23:18:13 +0800

    1

--- Mendel's_First_Law.c
+++ Mendel's_First_Law.c
@@ -1,28 +1,23 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
-
+#include<stdlib.h>
 /*
     Description of problem: 
     Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.  
     The probability that two randomly selected mating organisms will produce an individual possessing a dominant allele (and thus displaying the dominant phenotype). Assume that any two organisms can mate.
 */
-#include<stdio.h>
-#include<string.h>
-#include<math.h>
-
-float MFL(float, float, float);
-
+double MFL(double, double, double);
 int main(int argc, char const *argv[])
 {
     /*
     Simple definition of params of main program, k, m, and n is in order as description of problem said.  
     */
-    printf("%f", MFL(strtonum(argv[1]),strtonum(argv[2]),strtonum(argv[3])));
+    printf("%lf", MFL(strtod(argv[1],NULL),strtod(argv[2],NULL),strtod(argv[3],NULL)));
     return 0;
 }
 
-float MFL(float k, float m, float n){
+double MFL(double k, double m, double n){
     /*
         k is the homozygous dominant, m is the heterozygous,n is the homozygous recessive.  
     */
@@ -36,9 +31,9 @@ float MFL(float k, float m, float n){
     3. If aa in first order, the solution as the status of Aa in first order.  
     Thought 2:    
     */
-    float sm = k + m + n  
-    float kf = k/sm
-    float mf = (k * m) / (sm * (sm - 1)) + (m * (m - 1))/(2 * sm * (sm - 1)) + (m * n)/(4 * sm * (sm - 1))
-    float nf = (n * k) / (sm * (sm - 1)) + (n * m)/(4 * sm * (sm - 1))
+    double sm = k + m + n;
+    double kf = k/sm;
+    double mf = (k * m) / (sm * (sm - 1)) + (m * (m - 1))/(2 * sm * (sm - 1)) + (m * n)/(4 * sm * (sm - 1));
+    double nf = (n * k) / (sm * (sm - 1)) + (n * m)/(4 * sm * (sm - 1));
     return kf + mf + nf;
 }
---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<strings.h>
+#include<stdlib.h>
+#include<stab.h>
+
+
+int main(){
+	float d = (double)3/7.0;
+	printf("%f",d);
+}
