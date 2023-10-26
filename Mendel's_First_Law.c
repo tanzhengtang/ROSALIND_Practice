68a7fb46a5e3c1137f2be3fbc00f7c2699333653
TanZhengTang <tanzhengtang@163.com>
2023-10-26 11:34:04 +0800

    1

--- Mendel's_First_Law.c
+++ Mendel's_First_Law.c
@@ -1,3 +1,7 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
 /*
     Description of problem: 
     Three positive integers k, m, and n, representing a population containing k+m+n organisms: k individuals are homozygous dominant for a factor, m are heterozygous, and n are homozygous recessive.  
@@ -22,8 +26,13 @@ float MFL(int k, int m, int n){
     /*
         k is the homozygous dominant, m is the heterozygous,n is the homozygous recessive.  
     */
-   /*
+    /*
         The point is knowing that if you select an individual from the population, the number of the population and the number of the special subpopulation which the individual belong to will reduce too.
-   */
-   return (float)k
+    */
+    /*
+    1. If AA in first order, then whatever second mate is, their child will have a dominant allele.  
+    2. If Aa in first order, plus every status' probability together.  
+    3. If aa in first order, the solution as the status of Aa in first order.  
+    */  
+   return (float)k/(float)(k + m + n) + +((float)n/(float)(k + m + n)*(float));
 }
